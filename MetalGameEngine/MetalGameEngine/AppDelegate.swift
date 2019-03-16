8d8c95831812a672cf61c155ba62fb461789c42f
zhangsihuai <seahuai@foxmail.com>
2019-03-16 16:45:50 +0800

    Project initialize

---
+++ MetalGameEngine/MetalGameEngine/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  MetalGameEngine
+//
+//  Created by 张思槐 on 2019/3/16.
+//  Copyright © 2019 张思槐. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
