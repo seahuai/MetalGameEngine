92914ed5a5178c2d3615799c8b032132116e263f
zhangsihuai <seahuai@foxmail.com>
2019-04-15 21:21:33 +0800

    add 添加地形GUI

--- MetalGameEngine/GUI/Add/Terrain/AddTerrainViewController.swift
+++ MetalGameEngine/GUI/Add/Terrain/AddTerrainViewController.swift
@@ -10,16 +10,50 @@ import Cocoa
 
 class AddTerrainViewController: NSViewController {
 
+    @IBOutlet weak var inputTextfield: NSTextField!
+    @IBOutlet weak var positionInputView: VectorInputView!
+    @IBOutlet weak var warningLabel: NSTextField!
+    @IBOutlet weak var heightTextField: NSTextField!
+    @IBOutlet weak var sizeXTextField: NSTextField!
+    @IBOutlet weak var sizeZTextField: NSTextField!
+    @IBOutlet weak var previewLabel: NSTextField!
+    @IBOutlet weak var previewImageView: NSImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do view setup here.
+    
+        inputTextfield.delegate = self
     }
     
 }
 
-extension AddTerrainViewController: AddNodeVaildable {
-    var isVaild: Bool {
-        return false
+extension AddTerrainViewController: NSTextFieldDelegate {
+    func controlTextDidEndEditing(_ obj: Notification) {
+        let textFiled = obj.object as? NSTextField
+        if textFiled == inputTextfield {
+            warningLabel.stringValue = ""
+            let imageName = inputTextfield.stringValue
+            if !imageName.isEmpty {
+                if let image = NSImage(named: imageName) {
+                    previewImageView.image = image
+                } else {
+                    warningLabel.stringValue = "未找到 \(imageName)"
+                }
+            } else {
+                warningLabel.stringValue = "不得为空"
+            }
+        }
     }
+}
+
+extension AddTerrainViewController: AddNodeVaildable {
+    func checkVaild() -> (isVaild: Bool, errorMsg: String?) {
+        if let _ = previewImageView.image {
+            return (true, nil)
+        } else {
+            return (false, nil)
+        }
+    }
+    
 }
 
